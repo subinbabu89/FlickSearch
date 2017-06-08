350be8006d4c43073689b656c1b3045916a58bf8
subinbabu89 <subin.babu@mavs.uta.edu>
2017-06-08 03:42:39 -0500

    Add new images + added swipe refresh for the app page

--- app/src/main/java/self/coding/challenge/flicksearch/MainView.java
+++ app/src/main/java/self/coding/challenge/flicksearch/MainView.java
@@ -12,4 +12,19 @@ interface MainView {
      * @param allPhotos list of photos to be set
      */
     void setImagesToViewPager(List<IndivPhoto> allPhotos);
+
+    /**
+     * method used to display snackbar on the screen
+     */
+    void showSnackbar();
+
+    /**
+     * method used to dismiss snackbar from the screen
+     */
+    void dismissSnackBar();
+
+    /**
+     * Add swipe refresh listener to the app
+     */
+    void addSwipeRefreshListener();
 }
